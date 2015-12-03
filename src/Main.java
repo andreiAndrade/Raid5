a8405ebe72149cb575e0a35cdb008185f4aa491d
andreiAndrade <andrei.9551@gmail.com>
2015-12-03 00:52:43 -0200

    Código finalizado, fata a correção de alguns bugs, mas a logica esta pronta concluirei amanha

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TO DO
@@ -9,73 +11,34 @@ public class Main {
 
     //region Attributes
     private static final String PATH = "C:\\RAID5\\";
-    private static File disk0;
-    private static File disk1;
-    private static File parity;
-    private static File file;
+    private static final String DISK0 = "Disk0";
+    private static final String DISK1 = "Disk1";
+    private static final String PARITY = "Parity";
+    private static final String FILE0 = "File0.txt";
+    private static final String FILE1 = "File1.txt";
+    private static final String FILEPARITY = "FileParity.txt";
+    private static final String FILERECOVERED = "Arquivo Recuperado.txt";
+    private static PrintWriter w;
     //endregion
 
     //region PrivateMethods
-    //Converte o arquivo em bytes
-    private static byte[] parseFileToByte(File file){
 
-        int fileLength = (int)file.length();
-        byte[] bytesArray = new byte[fileLength];
-        FileInputStream fileLikeStream  = null;
+    //Cria diretórios que representaram os disco 0, 1 e de paridade REFATORADO
+    private static void createDisks(String... names){
+        File disk;
+        for (String name : names){
+            disk = new File(PATH, name);
+            if (!disk.exists()){
+                if (!disk.mkdirs()) System.out.printf("%s não pode ser criado!\n", name);
+                else System.out.printf("%s criado com sucesso!\n", name);
+            }
+        }
+    }
 
+    //Cria o arquivo original REFATORADO
+    private static File createFileOriginal(){
         try {
-
-            fileLikeStream = new FileInputStream(file);
-            fileLikeStream.read(bytesArray, 0, fileLength);
-
-        } catch (FileNotFoundException fnfex) {
-            System.out.println("Arquivo não encontrado!");
-        } catch (IOException ioex) {
-            System.out.println("Erro inesperado!");
-        }
-
-        return bytesArray;
-    }
-
-    //Cria diretórios que representaram os disco 0, 1 e de paridade
-    private static void createDisks(){
-
-        disk0 = new File(PATH + "Disk0");
-        disk1 = new File(PATH + "Disk1");
-        parity = new File(PATH + "Parity");
-
-        if (!disk0.exists()){
-            if (!disk0.mkdirs()) System.out.println("Disk0 não pode ser criado!");
-            else System.out.println("Disk0 criado com sucesso!");
-        }
-        if (!disk1.exists()){
-            if (!disk1.mkdirs()) System.out.println("Disk1 não pode ser criado!");
-            else System.out.println("Disk1 criado com sucesso!");
-        }
-        if (!parity.exists()){
-            if (!parity.mkdirs()) System.out.println("Parity não pode ser criado!");
-            else System.out.println("Parity criado com sucesso!");
-        }
-    }
-
-    //cria paridade dos discos
-    private static byte[] fillParity(byte[] disk0Byte, byte[] disk1Byte) {
-        byte[] parityByte = new byte[disk0Byte.length];
-        
-        for(int i = 0; i < parityByte.length; i++) {
-            parityByte[i] = disk0Byte[i] - disk1Byte[i];
-
-        }
-
-        return parityByte;
-    }
-    
-
-
-    //Cria o arquivo
-    private static void createFile(){
-        try {
-            file = new File(PATH + "Hino.txt");
+            File file = new File(PATH + "Hino.txt");
             PrintWriter w = new PrintWriter(file);
 
             w.println("Glória do desporto nacional");
@@ -111,21 +74,80 @@ public class Main {
             w.println("Colorado das glórias");
             w.println("Orgulho do Brasil");
             w.close();
-            System.out.println("Arquivo criado com sucesso!");
+            System.out.printf("Arquivo %s criado com sucesso!", file.getName());
+            return file;
         } catch (IOException e) {
             System.out.println("Não foi possível criar o arquivo solicitado!");
         }
+        return null;
     }
 
-    //Grava os arquivos de bytes nos discos
-    private static void writeFileLikeByteInDisk(byte[] arrayByte){
+    //Converte o arquivo em bytes REFATORADO
+    private static byte[] parseFileToByte(File file){
 
+        int fileLength = (int)file.length();
+        byte[] bytesArray = new byte[fileLength];
+        FileInputStream fileLikeStream  = null;
+
+        try {
+
+            fileLikeStream = new FileInputStream(file);
+            fileLikeStream.read(bytesArray, 0, fileLength);
+
+        } catch (FileNotFoundException fnfex) {
+            System.out.println("Arquivo não encontrado!");
+        } catch (IOException ioex) {
+            System.out.println("Erro inesperado!");
+        }
+
+        return bytesArray;
+    }
+
+    //Grava os arquivos de bytes nos discos REFATORADO
+    private static void saveFileWithRAID5(byte[] bytesArray){
+        //variaveis locais
+        boolean addFirstDisk = true;
+        List<Byte> file0 = new ArrayList<>();
+        List<Byte> file1 = new ArrayList<>();
+        List<Byte> fileParity = new ArrayList<>();
+
+        //preenche os discos com os arquivos de bytes
+        for(byte element : bytesArray) {
+            if (!addFirstDisk) file0.add(element);
+            else file1.add(element);
+            addFirstDisk = !addFirstDisk;
+        }
+
+        //Cria paridade
+        for (int i = 0; i < file1.size(); i++){
+            fileParity.add(
+                    (i == file0.size()) ? file1.get(i) : (byte) (file1.get(i) - file0.get(i))
+            );
+        }
+
+        fillBytesFile(file0, new File(PATH + DISK0, FILE0));
+        fillBytesFile(file1, new File(PATH + DISK1, FILE1));
+        fillBytesFile(fileParity, new File(PATH + PARITY, FILEPARITY));
+    }
+
+    //cria arquivo como byte. e chamado pelo saveFileWithRAID5 REFATORADO
+    private static void fillBytesFile(List<Byte> bytesArray, File file){
+        try {
+            w = new PrintWriter(file);
+            for (byte value : bytesArray) w.println(value);
+            w.close();
+            System.out.printf("Arquivo %s criado com sucesso!\n", file.getName());
+        } catch (FileNotFoundException e) {
+            System.out.println("Não foi possível criar o arquivo solicitado!");
+        }
     }
 
     //Apaga um disco para teste de recuperação da paridade
     private static void deleteDisk(){
 
         Scanner sc = new Scanner(System.in);
+        File disk0 = new File(PATH + DISK0);
+        File disk1 = new File(PATH + DISK1);
 
         System.out.println("--Remover disco--");
         System.out.println("Escolha o disco:");
@@ -137,6 +159,10 @@ public class Main {
         switch (option){
             case 0:
                 if (disk0.exists()) {
+                    String[] contents = disk0.list();
+                    for (String content : contents){
+                        new File(disk0, content).delete();
+                    }
                     if (!disk0.delete()) System.out.println("Disk0 não pode ser deletado!");
                     else System.out.println("Disk0 foi deletado com sucesso!");
                 } else{
@@ -144,7 +170,11 @@ public class Main {
                 }
                 break;
             case 1:
-                if (disk0.exists()) {
+                if (disk1.exists()) {
+                    String[] contents = disk1.list();
+                    for (String content : contents){
+                        new File(disk1, content).delete();
+                    }
                     if (!disk1.delete()) System.out.println("Disk1 não pode ser deletado!");
                     else System.out.println("Disk1 foi deletado com sucesso!");
                 } else {
@@ -155,99 +185,92 @@ public class Main {
 
     }
 
-    //Recupera o disco com o disco de paridade
-    private static byte[] recoverDisk(byte[] disk0, byte[] parity) {
-        byte[] recovered = new byte[disk0.length];
-        
-        for(int i = 0; i < parity.length; i++) {
-            recovered[i] = disk0[i] + parity[i];
+    //Recupera o disco com o disco de paridade REFATORADO
+    private static void findDiskCorrupted(){
+        File pathDisk0 = new File(PATH + DISK0);
+        File pathDisk1 = new File(PATH + DISK1);
 
+        if (pathDisk0.exists() && pathDisk1.exists()){
+            System.out.println("Nenhum disco esta corrompido!");
+        } else if (!pathDisk0.exists() && !pathDisk1.exists()){
+            System.out.println("Impossivel recuperar os arquivos. Os dois discos foram corrompidos!");
+        } else if (!pathDisk0.exists()){
+            recoveryDisk(DISK0, FILE0);
+        } else {
+            recoveryDisk(DISK1, FILE1);
         }
+    }
 
-        return recovered;
+    private static void recoveryDisk(String disk, String file){
+        try {
+            //recria disco apagado
+            createDisks(disk);
+            //recria arquivo apagado
+            File recoveredFile = new File(PATH + disk, FILERECOVERED);
+
+            Scanner readFile = new Scanner(new FileReader(new File(PATH + disk, file))).useDelimiter("\\n");
+            Scanner readFileParity = new Scanner(new FileReader(new File(PATH + disk, FILEPARITY))).useDelimiter("\\n");
+
+            byte byte0;
+            byte byteParity;
+            List<Byte> bytesArrayFile = new ArrayList<>();
+
+            while (readFileParity.hasNext()) {
+                byte0 = (readFile.hasNext()) ? Byte.parseByte(readFile.next().replace("\r", "")) : 0;
+                byteParity = Byte.parseByte(readFileParity.next().replace("\r", ""));
+                bytesArrayFile.add((file.equals(FILE0)) ? (byte) (byte0 - byteParity) : (byte) (byteParity + byte0));
+            }
+
+            fillBytesFile(bytesArrayFile, recoveredFile);
+            System.out.println("Arquivo recuperado com sucesso!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Remonta os arquivo
+    private static void remakeFile() {
+        List<Byte> fullFile = new ArrayList<>();
+        Scanner sc;
+        byte[] fullFileArray;
+        String[] contentDisk0 = new File(PATH + DISK0).list();
+        String[] contentDisk1 = new File(PATH + DISK1).list();
+        File file0 = new File(PATH + DISK0, contentDisk0[0]);
+        File file1 = new File(PATH + DISK1, contentDisk1[0]);
+
+        try {
+            sc = new Scanner(file0);
+            while (sc.hasNext()){
+                fullFile.add(Byte.parseByte(sc.next()));
+            }
+            sc = new Scanner(file1);
+            while (sc.hasNext()){
+                fullFile.add(Byte.parseByte(sc.next()));
+            }
+            fullFileArray = new byte[fullFile.size()];
+            for (int i = 0; i < fullFile.size(); i++){
+                fullFileArray[i] = fullFile.get(i);
+            }
+            String s = new String(fullFileArray);
+            System.out.println(s);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
     //endregion
 
-
     public static void main(String[] args) throws FileNotFoundException {
 
-        createFile();
+        createDisks(DISK0, DISK1, PARITY);
 
-        int fileLength = (int)file.length();
-        byte[] bytesArray = new byte[fileLength];
+        File fileOriginal = createFileOriginal();
+        byte[] fileLikeByte = parseFileToByte(fileOriginal);
+        saveFileWithRAID5(fileLikeByte);
+        deleteDisk();
+        findDiskCorrupted();
+//        remakeFile();
 
-        bytesArray = parseFileToByte(file);
-
-        int count = 0;
-
-        byte[] disk0Byte;
-        byte[] disk1Byte;
-        byte[] parityByte;
-
-        //dividing file in two different disks
-        for(byte element : bytesArray) {
-            if(count < (bytesArray.length/2)) {
-                disk0Byte[count] = element;
-            } else {
-                disk1Byte[count] = element;
-            }
-            count++;
-        }
-
-        //call method to fill the parity disk
-        parityByte = fillParity(disk0Byte, disk1Byte);
-
-        //write disks into real files
-        try {
-            for(byte element : disk0Byte) {
-                File disk0file = new File(PATH + disk0 + "disk0_byte" + element + ".txt");
-                PrintWriter w = new PrintWriter(disk0file);
-                w.println(element);
-                System.out.println("Arquivo criado com sucesso!");
-            }
-            
-        } catch (IOException e) {
-            System.out.println("Não foi possível criar o arquivo solicitado!");
-        }
-
-        try {
-            for(byte element : disk1Byte) {
-                File disk1file = new File(PATH + disk1 + "disk1_byte" + element + ".txt");
-                PrintWriter w = new PrintWriter(disk1file);
-                w.println(element);
-                System.out.println("Arquivo criado com sucesso!");
-            }
-            
-        } catch (IOException e) {
-            System.out.println("Não foi possível criar o arquivo solicitado!");
-        }
-
-
-        try {
-            for(byte element : parityByte) {
-                File parityfile = new File(PATH + parity + "parity_byte" + element + ".txt");
-                PrintWriter w = new PrintWriter(parityfile);
-                w.println(element);
-                System.out.println("Arquivo criado com sucesso!");
-            }
-            
-        } catch (IOException e) {
-            System.out.println("Não foi possível criar o arquivo solicitado!");
-        }
  
     }
-
-    // private static File writeDiskInFile(byte[] disk) {
-    //     try {
-    //         parity = new File(PATH + "parity.txt");
-    //         PrintWriter w = new PrintWriter(parity);
-    //         for(byte element : parityByte) {
-    //             w.println(element);
-    //         }
-    //         System.out.println("Arquivo criado com sucesso!");
-    //     } catch (IOException e) {
-    //         System.out.println("Não foi possível criar o arquivo solicitado!");
-    //     }
-        
-    // }
 }
+
